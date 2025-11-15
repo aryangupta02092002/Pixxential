bf83dca1e5fae02f917386051eb127a449f7c3ed
Aryan Gupta <aryangupta02092002@gmail.com>
2025-11-15 14:18:31 +0530

    backdrop added

--- app/page.js
+++ app/page.js
@@ -3,10 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello <br/>
-
-    <Button variant="primary">Button</Button>
-    <Button variant="glass">Button</Button>
+    <div>
 
     </div>
   );
