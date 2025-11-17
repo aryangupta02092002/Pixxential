5f43b393375a4b47d6ce205da91ff2cb78960b08
Aryan Gupta <aryangupta02092002@gmail.com>
2025-11-17 23:48:06 +0530

    Updated use-store-user for authentication

--- convex/users.js
+++ convex/users.js
@@ -42,11 +42,24 @@ export const store = mutation({
 });
 
 export const getCurrentUser = query({
-    handler: async (ctx) => {
-        const identity = await ctx.auth.getUserIdentity();
-        if (!identity) {
-            throw new Error("Not authenticated");
-        }
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new Error("Not authenticated");
     }
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier),
+      )
+      .unique();
+
+      if(!user){
+        throw new Error("User not found");
+      }
+
+      return user;
+
+  }
 }
 )
